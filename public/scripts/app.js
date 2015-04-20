8c66fbcebf476c8cfbe17735e63fb17b178aae0f
Kishan Shah <kishan.shah@gmail.com>
2015-04-20 15:01:46 -0500

    push

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function(event) {
-  //stuff happens here
+  console.log ('App has loaded')
+  var LogoView = Backbone.View.extend({
+  tagName: "div",
+  className: "column",
+  template: _.template("<h1><%= songName %>, spending cheese. <%= songName %>, on PC's. We doing <%= songName %> in WDI, its just Alex, James and Andy.</h1>"),
+  initialize: function() {
+    this.render();
+  },
+    render: function() {
+      var data = {
+        songName: "Big Pimpin"
+      };
+      var renderedTemplate = this.template(data);
+      this.$el.html(renderedTemplate);
+      console.log(this.$el);
+      $('body').append(this.$el);
+    }
+  })
+
+  var myFact = new LogoView();
 });
